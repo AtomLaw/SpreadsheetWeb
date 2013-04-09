24c4c181db40ae483b7e6f011ecaf3ee4420db46
Eric T Hurst <ehurst@lab1-1.eng.utah.edu>
2013-04-08 22:20:18 -0400

    added working timer example. will wait five seconds before printing Hello World

--- Server/main.cc
+++ Server/main.cc
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
+
+  boost::asio::io_service io;
+  
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
   cout << "Hello World" << endl;
 
   return 0;
