db440a61f7287fc366406f40af2c09b608bcd1d3
Evan Gould <egould@lab2-12.eng.utah.edu>
2013-04-22 15:22:22 -0600

    Added most of the undo functionality. Tell me if it is terrible or terribly wrong.

--- Server/session.cc
+++ Server/session.cc
@@ -48,52 +48,121 @@ void session::handle_message(Message msg, connection *conn)
       break;
     case MESSAGE_JOIN:
       break;
-    case MESSAGE_CHANGE:
+      case MESSAGE_CHANGE:
       if(ss->get_version() == msg.change.version)
-	{
-	  ss->update_cell(msg.change.cell, msg.change.content);
-	  ss->increment_version();
+      {
+       ss->update_cell(msg.change.cell, msg.change.content);
+       ss->increment_version();
 
-	  std::ostringstream out;
-	  out << "UPDATE\n"
-	      << "Name:" << msg.change.name << "\n"
-	      << "Version:" << ss->get_version() << "\n"
-	      << "Cell:" << msg.change.cell << "\n"
-	      << "Length:" << msg.change.content.length()<< "\n"
-	      << msg.change.content << "\n";
+       std::ostringstream out;
+       out << "UPDATE\n"
+       << "Name:" << msg.change.name << "\n"
+       << "Version:" << ss->get_version() << "\n"
+       << "Cell:" << msg.change.cell << "\n"
+       << "Length:" << msg.change.content.length()<< "\n"
+       << msg.change.content << "\n";
 
-	  std::vector<connection*>::iterator i;
-	  for(i = connections.begin(); i != connections.end(); i++)
-	    {
-	      (*i)->send_message(out.str());
-	    }
 
-	  std::ostringstream out_response;
-	  out_response << "CHANGE OK\n"
-		       << "Name:" << msg.change.name << "\n"
-		       << "Version:" << ss->get_version() << "\n";
-	  conn->send_message(out_response.str());
-	}
-      else
-	{
-	  std::ostringstream out;
-	  out << "CHANGE WAIT\n"
-	      << "Name:" << msg.change.name << "\n"
-	      << "Version:" << ss->get_version() << "\n";
-	  conn->send_message(out.str());
-	}
+      
+       std::vector<connection*>::iterator i;
+       for(i = connections.begin(); i != connections.end(); i++)
+       {
+         (*i)->send_message(out.str());
+       }
+
+       std::ostringstream out_response;
+       out_response << "CHANGE OK\n"
+       << "Name:" << msg.change.name << "\n"
+       << "Version:" << ss->get_version() << "\n";
+       conn->send_message(out_response.str());
+
+       //Add message to the undo stack
+       undo_oper operation;
+       operation.cell = msg.change.cell;
+       operation.contents = msg.change.content;
+       undo_stack.push_back(operation);
+     }
+     else
+     {
+       std::ostringstream out;
+       out << "CHANGE WAIT\n"
+       << "Name:" << msg.change.name << "\n"
+       << "Version:" << ss->get_version() << "\n";
+       conn->send_message(out.str());
+     }
 
       break;
     case MESSAGE_UNDO:
-      break;
-    case MESSAGE_SAVE:
-      {
-      this->ss->save();
+
+    //If there are no unsaved changes
+    if (undo_stack.size() == 0)
+    {
       std::ostringstream out;
-      out << "SAVE OK\n"
-	  << "Name:" << msg.save.name << "\n";
+      out << "UNDO END\n"
+      << "Name:" << msg.undo.name << "\n"
+      << "Version:" << ss->get_version() << "\n";
       conn->send_message(out.str());
+    } 
+    else if (ss->get_version() == msg.undo.version)
+    {
+
+      //Get the last change to the spreadsheet
+      undo_oper operation_to_undo = undo_stack.back();
+      undo_stack.pop_back();
+
+      //Update the cell in the spreadsheet
+      ss->update_cell(operation_to_undo.cell, operation_to_undo.contents);
+      ss->increment_version();
+
+      std::ostringstream out;
+      out << "UNDO OK\n"
+      << "Name:" << msg.undo.name << "\n"
+      << "Version:" << ss->get_version() << "\n"
+      << "Cell:" << operation_to_undo.cell << "\n"
+      << "Length:" << operation_to_undo.contents.length() << "\n"
+      << operation_to_undo.contents << "\n";
+      conn->send_message(out.str());
+
+      //Update all other clients
+      std::ostringstream out_msg;
+       out_msg << "UPDATE\n"
+       << "Name:" << msg.undo.name << "\n"
+       << "Version:" << ss->get_version() << "\n"
+       << "Cell:" << operation_to_undo.cell << "\n"
+       << "Length:" << operation_to_undo.contents.length()<< "\n"
+       << operation_to_undo.contents << "\n";
+
+
       
+       std::vector<connection*>::iterator i;
+       for(i = connections.begin(); i != connections.end(); i++)
+       {
+         (*i)->send_message(out_msg.str());
+       }
+
+
+    }
+    else //Client code is out of date
+    {
+      std::ostringstream out;
+      out << "UNDO WAIT\n"
+      << "Name:" << msg.undo.name << "\n"
+      << "Version:" << ss->get_version() << "\n";
+      conn->send_message(out.str());
+
+    }
+      break;
+
+      case MESSAGE_SAVE:
+      {
+        this->ss->save();
+        std::ostringstream out;
+        out << "SAVE OK\n"
+        << "Name:" << msg.save.name << "\n";
+        conn->send_message(out.str());
+      
+        //Clear the undo stack?
+        undo_stack.clear();
       }
       break;
     case MESSAGE_LEAVE:
